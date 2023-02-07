d9d284933e38027340a6fc26f4db835173fd740c
edunyas <dunyaecemm@gmail.com>
2023-02-07 12:24:14 +0300

    dependency injection applied

---
+++ DevTrust/DevTrustUnitTest/DataAccessServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using DevTrust.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace DevTrust.Tests
+{
+    [TestClass]
+    public class DataAccessServiceTests
+    {
+        private IDataAccessService _service;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _service = new DataAccessService();
+        }
+
+        [TestMethod]
+        public void GetPeople_ReturnsListOfPeople()
+        {
+            // Act
+            var result = _service.Get
